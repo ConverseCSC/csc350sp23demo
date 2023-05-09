10a2384546e77846110a7b419997910c42d06966
Peter Brown <phbrown@acm.org>
2023-05-09 13:54:18 -0400

    Syntax: where, let, case

--- Haskell/syntax.hs
+++ Haskell/syntax.hs
@@ -1,4 +1,4 @@
-import System.Win32 (xBUTTON1, aCCESS_SYSTEM_SECURITY, dACL_SECURITY_INFORMATION)
+import System.Win32 (xBUTTON1, aCCESS_SYSTEM_SECURITY, dACL_SECURITY_INFORMATION, SECURITY_ATTRIBUTES (nLength))
 import Text.XHtml (cite)
 --- PATTERN MATCHING
 
@@ -69,3 +69,33 @@ fastexp' x y
     | y < 0 = 1 / fastexp' x (-y)
     | y > 0 && even y = fastexp' x (y `div` 2) ^ 2
     | y > 0 && odd y = x * fastexp' x (y `div` 2) ^ 2
+
+---------------- Where
+
+fib :: (Integral a) => a -> a
+fib n
+    | n < 2 = n
+    | n >= 2 = fib (n - 1) + fib (n - 2)
+
+fib' :: (Integral a) => a -> a
+fib' n
+    | n < 2 = n
+    | n >= 2 = fst (fastfib n) -- fastfib defined in where
+    where fastfib n  -- where can define functions as well as named values
+            | n < 2 = (n, 0)
+            | n >= 2 = (fst prevfib + snd prevfib, fst prevfib)
+            where prevfib = fastfib (n - 1) 
+
+---------------- Let
+
+afterTax :: (Integral a, Fractional b) => a -> b
+afterTax income = let taxRate = fromIntegral((income + 999) `div` 1000) * 0.005
+                      fracIncome = fromIntegral(income)
+                      tax = fracIncome * taxRate
+                  in fracIncome - tax
+
+--------------- Case
+
+length'' :: [a] -> Int
+length'' xs = case xs of [] -> 0
+                         (_:xs2) -> 1 + length'' xs2
